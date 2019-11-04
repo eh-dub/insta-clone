f995bf209881689f3fb3b939ec2d4e9b21a53805
asweingarten <asweingarten@gmail.com>
2019-11-04 11:58:58 -0800

    Sapper project that can deploy to now and with insta data

---
+++ src/session-data.js
@@ -0,0 +1,89 @@
+export const posts = [
+  {
+    user: 'healthybts',
+    caption: `Fluffy chocolate chip pancakes layered with banana slices and topped with a chocolate drizzle, raspberries, and cacao powder. Who’s a chocolate lover?
+
+    1/2 c oats
+    1/2 medium banana
+    1/2 teaspoon vanilla extract
+    1 tsp baking powder
+    1/2 tsp cinnamon
+    1 egg
+    1 tbs almond milk
+    blend ingredients
+    mix in tsp apple cider vinegar
+    Add on chocolate chips while cooking
+    `,
+    comments: [
+      {
+        user: "panaceas_pantry",
+        comment: "Omg that is looking out of this world 🤤🤤"
+      },
+      {
+        user: "healthybts",
+        comment: `
+        #foodie #thefeedfeed #healthy #healthyfood #workoutmotivation #healthymotivation #eatclean #cleaneating #tasty #nutrition #toneitup
+#healthyinspo #food52grams #food52 #instagood #buzzfeedfood #foodstagram #yummy #food #cleanliving #healthandwellness #thektchen #wholefoods #cleanfoods
+        `,
+
+      }
+    ]
+  },
+  {
+    user: 'panaceas_pantry',
+    caption: `
+    No-bake choc hazelnut and coconut slice. Super simple, staple ingredients, totally yum! . .
+    If you don’t have/like hazelnuts you could use pecans instead- or even almonds (but that would be my last choice- and you might need extra sweetener). I used vegan milk choc for the topping, and wouldn’t recommend subbing for a darker choc unless you add extra sweetener in- it’s needed to balance out the flavours x
+    .
+    Base
+    .
+    1 cup (activated or freshly roasted) hazelnuts
+    2 Tbsp coconut flour
+    1/4 cup raw cacao powder
+    1/4 cup pure maple syrup
+    2 Tbsp coconut oil, melted
+    .
+    1. Add nuts, flour and cacao to a food processor, and blitz until nuts are finely chopped.
+    2. Add remaining ingredients and pulse to combine well. Press into the base of a small slice tin lined with baking powder (mine was 15cm square). Set aside.
+    .
+    Coconut layer.
+    .
+    3 cups shredded coconut
+    1 tsp vanilla extract
+    1/4 cup coconut oil, melted
+    1/4 cup maple syrup
+    1/4 cup coconut milk
+    .
+    1. Add all ingredients to a bowl and combine. Pour over base and set in fridge.
+    .
+    Choc ganache topping
+    .
+    100g vegan milk chocolate** 3 Tbsp coconut milk.
+    .
+    1. Finely chop up chocolate and set in a bowl (the finer, the better). Heat coconut milk (until hot, but NOT boiling point), pour over chocolate and whisk together. Pour over coconut layer, then set in the fridge. *Store in an airtight container, in the fridge/freezer. ** I would recommend using dark chocolate, as the extra sweetness is needed for this slice. If you DO use dark chocolate, then I’d recommend adding sweetener.`,
+    comments: [
+      {
+        user: "panaceas_pantry",
+        comment: `#foodphotography #foodphoto #foodstylist #veganinspo #veganrecipe #healthyvegan #healthyveganfood #veganblogger #veganvibes #chocolatey #nobake`
+      },
+      {
+        user: "healthybts",
+        comment: "These look so yummy 😍"
+      }
+    ]
+
+  }
+];
+
+export const profiles = {
+  "healthybts": {
+    bio: `A healthy living
+    📍LA`
+  },
+  "panaceas_pantry": {
+    bio: `🍄 Simple, plant-filled Wholefood recipes
+    📷 Food Photographer + naturopath
+    ✉️ work Enquiries: panaceas.pantry@gmail.com`,
+    website: "www.panaceaspantryblog.com"
+  }
+}
